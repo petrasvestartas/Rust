d6883ef87bbf1d35f00184b6916b43fdfebe574d
petras <petrasvestartas@gmail.com>
2024-11-30 20:06:49 +0100

    REFACTORING

--- HelloWorld/src/main.rs
+++ HelloWorld/src/main.rs
@@ -6,14 +6,14 @@ struct MigrosBag {
 fn main() {
 
     // Lists
-    let types = vec!["round", "triangle", "quad", "cylindrical"];
+    let cake_types = vec!["round", "triangle", "quad", "cylindrical"];
     let values = vec!["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "B", "C", "D"];
 
     // Loops
     let mut cakes = vec![];
-    for suit in &types {
+    for cake_type in &cake_types {
         for value in &values {
-            let cake = format!("{} of {}", value, suit);
+            let cake = format!("{} of {}", value, cake_type);
             cakes.push(cake);
         }
     }
