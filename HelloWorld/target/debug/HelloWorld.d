2dbb9348441005cdebf526c35bbe936335f08ebb
petras <petrasvestartas@gmail.com>
2024-11-30 18:41:20 +0100

    ADD new project hellow world

---
+++ HelloWorld/target/debug/HelloWorld.d
@@ -0,0 +1,1 @@
+/home/petras/code/rust/Rust/HelloWorld/target/debug/HelloWorld: /home/petras/code/rust/Rust/HelloWorld/src/main.rs
---
+++ HelloWorld/target/debug/deps/HelloWorld-7a2ab28643d3776a.d
@@ -0,0 +1,5 @@
+/home/petras/code/rust/Rust/HelloWorld/target/debug/deps/HelloWorld-7a2ab28643d3776a: src/main.rs
+
+/home/petras/code/rust/Rust/HelloWorld/target/debug/deps/HelloWorld-7a2ab28643d3776a.d: src/main.rs
+
+src/main.rs:
