d7facc61d0042bc3ff7f36d3064a959fa3452979
petras <petrasvestartas@gmail.com>
2024-11-30 20:03:06 +0100

    ADD struct, list, loop and print declarations.

--- HelloWorld/src/main.rs
+++ HelloWorld/src/main.rs
@@ -1,3 +1,26 @@
+#[derive(Debug)]
+struct MigrosBag {
+    cakes: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    // Lists
+    let types = vec!["round", "triangle", "quad", "cylindrical"];
+    let values = vec!["2", "3", "4", "5", "6", "7", "8", "9", "10", "A", "B", "C", "D"];
+
+    // Loops
+    let mut cakes = vec![];
+    for suit in &types {
+        for value in &values {
+            let cake = format!("{} of {}", value, suit);
+            cakes.push(cake);
+        }
+    }
+
+    // Struct instance
+    let migros_bag: MigrosBag = MigrosBag { cakes };
+
+    // Print formatting
+    println!("Here is your MigrosBag you paid 0.4 CHF: {:#?}", migros_bag);
 }
